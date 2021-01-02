2795f4cc76aec3d9f5453e7d90a70d4abcf6c332
Linda <lindathehuang@gmail.com>
2021-01-02 12:27:34 -0800

    Removed IE compatibility, updated playground, updated darkmode (still not functional)

--- stylescripts/script.js
+++ stylescripts/script.js
@@ -1,21 +1,24 @@
-var lightmode = true; //lightmode = default
+var lightmode; //lightmode = default
 
 $(document).ready(function() { //JQuery code goes here
     setDarkmode();
     $("#confetti").click(function() { confetti.start(1000, 100); });
-
+    
     function setDarkmode() {
+        lightmode = sessionStorage.getItem("darkmode");
         $(".darkmode").click(function() { //Darkmode
-            $("body").toggleClass("dark")
             lightmode = !lightmode;
             if (lightmode) {
+                $("body").removeClass("dark")
                 $("#logo").attr("src", "resources/logo-black.png");
                 $("#sublogo").attr("src", "../resources/logo-black.png");
             } else {
+                $("body").addClass("dark")
                 $("#logo").attr("src", "resources/logo-white.png");
                 $("#sublogo").attr("src", "../resources/logo-white.png");
             }
             projectScrolled();
+            sessionStorage.setItem("darkmode", $("body").hasClass("dark"));
         });
     }
 
